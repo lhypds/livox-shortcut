097abc16123d621dd01d4c838f86c982d03776b9
liu <lhypds@gmail.com>
2021-07-27 10:32:43 +0900

    Update shortcut

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
-# Livox Shortcut
 
-Just works
+Livox Shortcut
+==============
+
+
+LiDAR Device List
+-----------------
+
+3JEDHC900100491  Base     Center
+3JEDJ5Q00100431  Target1  Back
+3JEDJ6400100191  Target2  Right
+3JEDJ6400100241  Target3  Back Right
+3JEDJ6400100291  Target4  Back Left
+3JEDJ6400100591  Target5  Left
+
+
+Record ROS Bag
+--------------
+
+* Method 1
+
+1. Record lvx file
+2. ros-driver-lvx-to-rosbag
+
+* Method 2
+
+1. ros-drive-recieve-data
+2. Use ros-rqt.sh to record
+
+
+Auto Calibration
+----------------
+
+1. Record ros bag
+3. ros-rosbag-to-pcd(use auto calibraton)
+4. auto-calibration.sh
+
+
+
--- ros-rosbag-to-pcd/ros-bag-to-pcd-for-auto-calibration.sh
+++ ros-rosbag-to-pcd/ros-bag-to-pcd-for-auto-calibration.sh
@@ -3,8 +3,8 @@ rm /home/liu/livox/github-livox-sdk/Livox_automatic_calibration/data/Target-LiDA
 tmux new-session -d -s "roscore" roscore
 sleep 2
 rm /home/liu/Desktop/out/*
-rosbag filter '/home/liu/Desktop/Experiment_20210707/test11.bag' '/home/liu/Desktop/out/topic891.bag' "topic == '/livox/lidar_3JEDHB300100891'"
-rosbag filter '/home/liu/Desktop/Experiment_20210707/test11.bag' '/home/liu/Desktop/out/topic641.bag' "topic == '/livox/lidar_3JEDHB300100641'"
-rosrun pcl_ros bag_to_pcd /home/liu/Desktop/out/topic891.bag /livox/lidar_3JEDHB300100891 /home/liu/livox/github-livox-sdk/Livox_automatic_calibration/data/Base_LiDAR_Frames
-rosrun pcl_ros bag_to_pcd /home/liu/Desktop/out/topic641.bag /livox/lidar_3JEDHB300100641 /home/liu/livox/github-livox-sdk/Livox_automatic_calibration/data/Target-LiDAR-Frames
+rosbag filter '/home/liu/Desktop/Experiment_20210721/test1.bag' '/home/liu/Desktop/out/topic_base.bag' "topic == '/livox/lidar_3JEDJ6400100591'"
+rosbag filter '/home/liu/Desktop/Experiment_20210721/test1.bag' '/home/liu/Desktop/out/topic_target.bag' "topic == '/livox/lidar_3JEDHC900100491'"
+rosrun pcl_ros bag_to_pcd /home/liu/Desktop/out/topic_base.bag /livox/lidar_3JEDJ6400100591 /home/liu/livox/github-livox-sdk/Livox_automatic_calibration/data/Base_LiDAR_Frames
+rosrun pcl_ros bag_to_pcd /home/liu/Desktop/out/topic_target.bag /livox/lidar_3JEDHC900100491 /home/liu/livox/github-livox-sdk/Livox_automatic_calibration/data/Target-LiDAR-Frames
 tmux kill-server
