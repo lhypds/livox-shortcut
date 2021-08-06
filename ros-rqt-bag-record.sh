7b2ecae957a1af6b0a389f370abecf37fbb28952
LIU Heyang <lhypds@gmail.com>
2021-08-06 11:06:25 +0900

    rqt bag record

---
+++ ros-rqt-bag-record.sh
@@ -0,0 +1,7 @@
+tmux new-session -d -s "roscore" roscore
+sleep 2
+tmux new-session -d -s "recieve"
+gnome-terminal -x bash -c "tmux attach -t "recieve"; exec bash && exit"
+tmux send-key -t "recieve" 'bash '/home/liu/Desktop/livox-shortcut/ros-driver-recieve-data/livox-ros-driver-launch-livox-hub-multi-topic.sh'' Enter
+rqt_bag
+tmux kill-session
--- ros-rqt-bag.sh
+++
@@ -1,4 +0,0 @@
-tmux new-session -d -s "roscore" roscore
-sleep 2
-rqt_bag
-tmux kill-session
